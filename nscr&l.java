69f8fdd30c095dd5f0a82c2d9ff15a1503080013
ThanyaV <thanyavenkat1125@gmail.com>
2025-02-12 11:53:27 +0530

    Add files via upload
    non short circuit relational and logical operator

---
+++ nscr&l.java
@@ -0,0 +1,14 @@
+class Main
+{ 
+    public static void main (String s[])
+    {
+       int a=10,b=20,c=9;
+       
+       if(a>=b&a>=c)
+          System.out.println(a+ "is the greatest");
+       else if(b>=a&b>=c)
+          System.out.println(b+ "is the greatest");
+       else
+          System.out.println(c+ "is the greatest");
+    }
+}    
