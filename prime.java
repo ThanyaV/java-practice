e3b5a2c58162f2f9db3d3db79af0d39d28c639b1
ThanyaV <thanyavenkat1125@gmail.com>
2025-02-12 15:37:15 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,24 @@
+class Main
+{ 
+     static boolean isprime(int n)
+    {
+        if (n<=1)
+        return false;
+        for(int i=2;i<n;i++)
+        if(n%i==0)
+         return false;
+         return true;
+        
+    }
+    public static void main(String args[])
+    {
+        if (isprime(11))
+          System.out.println("true");
+        else
+           System.out.println("false");
+        if(isprime(3))
+            System.out.println("true");
+        else
+           System.out.println("false");
+    }
+}
