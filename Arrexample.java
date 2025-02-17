d22bc3b1896b5aa02d6a599fe81c9281c297b221
ThanyaV <thanyavenkat1125@gmail.com>
2025-02-17 09:40:07 +0530

    Add files via upload

---
+++ Arrexample.java
@@ -0,0 +1,12 @@
+class Arrexample
+{
+    public static void main(String arg[])
+    {
+        int arr[]={1,2,3,4,5};
+        int i;
+        for(i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
