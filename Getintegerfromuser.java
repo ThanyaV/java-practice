fa69469bf75a8a881c8bad61ce93fc01a5bb9de1
ThanyaV <thanyavenkat1125@gmail.com>
2025-02-19 09:51:57 +0530

    Add files via upload

---
+++ Getintegerfromuser.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Getintegerfromuser
+{
+    public static void main(String arg[])
+    {
+        Scanner in =new Scanner(System.in);
+        System.out.print("Enter a Integer:");
+        int a = in.nextInt();
+        System.out.println("Integer:"+a);
+        
+    }
+}
