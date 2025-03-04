f3a9da9fd48742c83f930cc5f93659fa15a4cc81
ThanyaV <thanyavenkat1125@gmail.com>
2025-03-04 15:17:18 +0530

    Add files via upload

--- Reverse_words.java
+++ Reverse_words.java
@@ -6,13 +6,14 @@ class Reverse_words
         Scanner in=new Scanner(System.in);
         System.out.print("Enter the string : ");
         String a=in.nextLine();
+        a=a.replaceAll("\\s+"," ").trim();
         String[] w = a.split(" ");
         StringBuilder reverse=new StringBuilder();
         for(int i=w.length-1;i>=0;i--)
         {
             reverse.append(w[i]).append(" ");
         }
-        System.out.println("The reversed Sentence : "+reverse.toString());
+        System.out.println("The reversed Sentence : "+reverse.toString().trim());
         
     }
 }
